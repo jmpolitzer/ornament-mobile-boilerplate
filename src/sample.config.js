17582c69cb81e0a3ef410f7fbff96a163856879c
Jay Politzer <jmpolitzer@gmail.com>
2018-02-21 17:25:27 -0600

    adds sample config file for easier cloning

---
+++ src/sample.config.js
@@ -0,0 +1,10 @@
+/* Replace the sample below with your Firebase credentials and rename this file config.js */
+
+module.exports = {
+  FIREBASE_API_KEY: '<API_KEY>',
+  AUTH_DOMAIN: '<PROJECT_ID>.firebaseapp.com',
+  DATABASE_URL: 'https://<DATABASE_NAME>.firebaseio.com',
+  PROJECT_ID: '<PROJECT_ID>',
+  STORAGE_BUCKET: '<BUCKET>.appspot.com',
+  FIREBASE_MESSAGING_SENDER_ID: '<SENDER_ID>',
+};
