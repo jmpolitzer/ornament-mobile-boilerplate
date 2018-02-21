38980830a6a078a17ff8bf2441df1cce4fc85655
Jay Politzer <jmpolitzer@gmail.com>
2018-02-20 20:37:27 -0600

    fixes broken react-navigation dep

--- src/containers/main/index.js
+++ src/containers/main/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { addNavigationHelpers } from 'react-navigation';
+import { createReduxBoundAddListener } from 'react-navigation-redux-helpers';
 import { RootNavigator } from '../../navigation';
+
+const addListener = createReduxBoundAddListener('root');
 
 class Main extends React.Component {
   render() {
     return (
       <RootNavigator navigation={addNavigationHelpers({
         dispatch: this.props.dispatch,
-        state: this.props.navigation
+        state: this.props.navigation,
+        addListener
       })} />
     );
   }
