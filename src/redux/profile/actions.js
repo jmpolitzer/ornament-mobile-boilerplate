41cd7a885c692361a5d13c06a2a009b353838b66
Jay Politzer <jmpolitzer@gmail.com>
2018-03-27 16:47:30 -0500

    implements beginning of ImagePicker

--- src/redux/profile/actions.js
+++ src/redux/profile/actions.js
@@ -1,18 +1,37 @@
+import * as Constants from './constants';
 import { ImagePicker } from 'expo';
 
 export function selectProfilePhoto() {
-  console.log(ImagePicker);
-  return dispatch => {
-    let result = ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      allowsEditing: true,
-      aspect: [3, 3]
-    });
+  return async dispatch => {
+    try {
+      let data = await pickImage();
 
-    console.log(result);
+      if(!data.cancelled) {
+        console.log('image', data.uri);
 
-    return {
-      type: 'THING'
+        return {
+          type: 'SELECT_IMAGE'
+        }
+      } else {
+        return {
+          type: 'CANCEL_SELCECT_IMAGE'
+        }
+      }
+    } catch(e) {
+      console.log('error', e);
+      return {
+        type: 'IMAGE_PICKER_ERROR'
+      }
     }
   }
+}
+
+async function pickImage() {
+  let result = await ImagePicker.launchImageLibraryAsync({
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    allowsEditing: true,
+    aspect: [3, 3]
+  });
+
+  return result;
 }
