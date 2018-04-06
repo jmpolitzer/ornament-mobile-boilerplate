e9a63e8da4d9fbbd0e406dadb42a83cd68da706d
Jay Politzer <jmpolitzer@gmail.com>
2018-04-06 11:04:22 -0500

    adds notifications tab

---
+++ src/containers/notifications/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { View, Text, StyleSheet } from 'react-native';
+
+class Notifications extends React.Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Text>
+          Notifications Tab
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+    alignItems: 'center'
+  }
+});
+
+const mapStateToProps = state => {
+  return {
+    signedInUser: state.auth.signedInUser
+  };
+}
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+
+}, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Notifications);
--- src/navigation/signedInTabs.js
+++ src/navigation/signedInTabs.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { RecipesStack } from './recipesStack';
 import Profile from '../containers/profile';
-
+import Notifications from '../containers/notifications';
 
 export const SignedInTabs = TabNavigator({
   RecipesStack: {
@@ -14,5 +14,12 @@ export const SignedInTabs = TabNavigator({
         screen: Profile
       }
     })
-  }
+  },
+  NotificationsStack: {
+    screen: StackNavigator({
+      Notifications: {
+        screen: Notifications
+      }
+    })
+  },
 });
